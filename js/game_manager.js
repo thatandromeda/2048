cb06bf38d8b70e239721f26ee4250c5195636ead
Andromeda Yelton <andromeda.yelton@gmail.com>
2015-08-25 10:06:28 -0400

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -161,7 +161,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value * tile.value);
+          var merged = new Tile(positions.next, tile.value * 3);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
