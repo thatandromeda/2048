b90e1ab0a9ccdf99d92534fc323daa44d4b11226
Andromeda Yelton <andromeda.yelton@gmail.com>
2015-08-25 09:28:16 -0400

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -70,11 +70,11 @@ GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     var randomizer = Math.random();
     if (randomizer < 0.9 ) {
-      var value = 2;
+      var value = 3;
     } else if (randomizer < 0.994) {
-      var value = 6;
+      var value = 9;
     } else {
-      var value = 1024;
+      var value = 27;
     }
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
